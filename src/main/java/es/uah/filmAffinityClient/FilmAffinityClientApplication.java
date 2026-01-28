ac2d7726a97441e7dc212a28c09d7b0ce77a7a65
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2026-01-28 20:07:17 +0100

    First commit

---
+++ src/main/java/es/uah/filmAffinityClient/FilmAffinityClientApplication.java
@@ -0,0 +1,13 @@
+package es.uah.filmAffinityClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmAffinityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmAffinityClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uah/filmAffinityClient/FilmAffinityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.filmAffinityClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmAffinityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
